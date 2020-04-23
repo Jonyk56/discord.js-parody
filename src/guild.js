aa033a58ed93bada69951a0afb0e2d283ba55f66
Jonyk52 <jtechmaster3@gmail.com>
2020-04-23 12:55:17 -0400

    update - src/guild.js -> +GHUB class ( extends GuildManager )

--- src/guild.js
+++ src/guild.js
@@ -20,4 +20,36 @@ const {
  * @since 0.1.16-beta
  */
 
- 
+/**
+ * guild hub
+ * @since 0.2.0-beta
+ * @class GHUB
+ */
+
+module.exports.GHUB = class extends GuildManager {
+  /**
+   * * A Twitter snowflake, except the epoch is 2015-01-01T00:00:00.000Z
+   * ```
+   * If we have a snowflake '266241948824764416' we can represent it as binary:
+   *
+   * 64                                          22     17     12          0
+   *  000000111011000111100001101001000101000000  00001  00000  000000000000
+   *       number of ms since Discord epoch       worker  pid    increment
+   * ```
+   * @typedef {string} Snowflake
+   */
+  /**
+   * @typedef {Collection<Snowflake,Guild>} GuildCollection
+   * @description Collection of guilds
+   */
+  /**
+   * Get size of Client guild count OR GuildInstance count
+   * @param {GuildCollection|Array} GuildInstance
+   */
+  static GetSize(GuildInstance = []) {
+    if (GuildInstance instanceof Array){
+      GuildInstance = this.guilds.cache
+    }
+    
+  }
+};
